66cdd889a24a169d710bba46b08d77af8eddf324
LDG0520 <113489359+LDG0520@users.noreply.github.com>
2022-09-14 20:43:20 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,57 +1,25 @@
 input.onButtonPressed(Button.A, function () {
-    gamesteps = 0
-    for (let index = 0; index < 3; index++) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # # # .
-            # . . . #
-            `)
-    }
-    basic.clearScreen()
-    basic.pause(1000)
-    basic.showString("GAME START")
-    random = randint(10, 50)
-    basic.showNumber(random)
-    if (0 >= random) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            `)
-    } else {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . # # # .
-            # . . . #
-            `)
-    }
+    basic.showString("How many steps?")
+    basic.showNumber(steps)
 })
 input.onGesture(Gesture.Shake, function () {
     steps += 1
-    basic.showNumber(steps)
-    gamesteps += 1
+    for (let index = 0; index < 2; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    }
 })
 input.onButtonPressed(Button.AB, function () {
     steps = 0
     basic.showString("RESET")
 })
 input.onButtonPressed(Button.B, function () {
+    if (steps >= 1500) {
+        music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+    } else {
+        music.startMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.Once)
+    }
     basic.showNumber(0.7 * steps)
 })
-let random = 0
-let gamesteps = 0
 let steps = 0
 steps = 0
